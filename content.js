19591671ec391a4e69816be710e70967314ca958
ColtonFindley <123335067+ColtonFindley@users.noreply.github.com>
2025-04-08 20:12:54 -0600

    Update content.js to version 3.1

--- content.js
+++ content.js
@@ -1,11 +1,27 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.command === "getCurrentTime") {
+    chrome.storage.local.get({ lastTimestamp: 0 }, ({ lastTimestamp }) => {
+      chrome.runtime.sendMessage({ type: "UPDATE_TIMESTAMP", timestamp: lastTimestamp });
+    });
+  }
+});
+
+function sendTimestampUpdate() {
+    const player = document.querySelector('.html5-video-player');   // main wrapper
+    const isAd = player && (
+        player.classList.contains('ad-showing') ||      // pre‑/mid‑roll
+        player.classList.contains('ad-interrupting')    // some older skins
+    );
+    if (isAd) {
+      return; // <-- no further messages
+    }
+ 
     const videoElement = document.querySelector("video");
     if (videoElement) {
       const tmsp = Math.floor(videoElement.currentTime);
-      chrome.runtime.sendMessage({ type: "UPDATE_TIMESTAMP", timestamp: tmsp });
-    }else{
-      chrome.runtime.sendMessage({ type: "UPDATE_TIMESTAMP", timestamp: "none" });
+      chrome.storage.local.set({ lastTimestamp: tmsp });
     }
-  }
-});
+};
+
+// Continuously check for updates every half second
+setInterval(sendTimestampUpdate, 500);
