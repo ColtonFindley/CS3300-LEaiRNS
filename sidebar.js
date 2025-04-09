ec42574b6ae5a5a9138bb5ed6297f8e65211257a
ColtonFindley <123335067+ColtonFindley@users.noreply.github.com>
2025-04-08 19:21:39 -0600

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,3 +1,15 @@
+// Close sidebar when button is clicked
 document.getElementById("closeBtn").addEventListener("click", () => {
     document.getElementById("sidebar").style.display = "none";
-});  
+});
+
+document.getElementById("timestampBtn").addEventListener("click", async () => {
+    // Find the active tab in the current window
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    //console.log("Sending tab", tab.id, tab.url)
+
+    if (!tab) return;
+
+    // Send the message directly to the content script in that tab
+    chrome.tabs.sendMessage(tab.id, { command: "getCurrentTime" });
+});
