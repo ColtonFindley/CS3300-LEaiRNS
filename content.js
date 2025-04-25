db0a0c4aecfb7ec28a44fad20de15bc9cf8dd369
danhettich <81657926+danhettich@users.noreply.github.com>
2025-04-24 20:00:22 -0400

    clarifying comments content.js

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+
+
+//This content script pulls the timestamp from the video element of the page and saves it
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.command === "getCurrentTime") {
     const videoElement = document.querySelector("video");
@@ -9,19 +12,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 function sendTimestampUpdate() {
-    const player = document.querySelector('.html5-video-player');   // main wrapper
+    const player = document.querySelector('.html5-video-player');   //gets the video element
     const isAd = player && (
-        player.classList.contains('ad-showing') ||      // pre‑/mid‑roll
-        player.classList.contains('ad-interrupting')    // some older skins
+        player.classList.contains('ad-showing') ||      // checks if the video content currently showing is an ad
+        player.classList.contains('ad-interrupting')    
     );
     if (isAd) {
-      return; // <-- no further messages
+      return; //do not save the current timestamp if it is an ad
     }
  
     const videoElement = document.querySelector("video");
     if (videoElement) {
       const tmsp = Math.floor(videoElement.currentTime);
-      chrome.storage.session.set({ lastTimestamp: tmsp });
+      chrome.storage.session.set({ lastTimestamp: tmsp }); 
+      //use chrome.storage.session instead of .local and things saved with .session are cleared between uses while .local is not
+      //This can cause an error due to saving something when there is no guarantee that the extension still exists
+      //This error does not impact functionality. It might need to be fixed if you plan to modify this code and actually upload it to the chrome web store
     }
 };
 
